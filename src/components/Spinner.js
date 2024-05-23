7ae758b9421260bb2b8d0064461f116d3368a38d
piyushparmar911 <prparmar911@gmail.com>
2024-05-23 08:42:21 +0530

    content changed

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -5,7 +5,7 @@ export default function Spinner() {
   return (
     <div>
       <div className='justify-content-center d-flex my-3' >
-        <img src={spiner} alt="Loading"  style={{height: "150px", width: "150px" }}/>
+        <img src={spiner} alt="Loading"  style={{height: "120px", width: "150px" }}/>
       </div>
       
     </div>
